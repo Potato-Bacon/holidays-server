c92f0af2689185278c571fd4ecd596ab4daf4496
potatobacon <andylohbiz@gmail.com>
2022-09-12 10:33:08 +0800

    server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT ?? 3100;
+const port = process.env.PORT ?? 3100;
 
 app.get("/", (req, res) => {
   res.send({ msg: "Holidays" });
